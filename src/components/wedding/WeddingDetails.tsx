6080b5acc695ec382dedaf75afda165375c5bf92
Andreas Olsson <58911738+adde53@users.noreply.github.com>
2026-01-11 17:29:54 +0100

    Update WeddingDetails.tsx

--- src/components/wedding/WeddingDetails.tsx
+++ src/components/wedding/WeddingDetails.tsx
@@ -8,14 +8,14 @@ const WeddingDetails = () => {
       title: "Vigsel",
       time: "14:30",
       location: "Lidingö kyrka",
-      address: "Stockholmsvägen 16, Lidingö",
+      address: "Kyrkallén 4, Lidingö",
     },
     {
       icon: PartyPopper,
       title: "Fest & Middag",
       time: "ca 16:00",
       location: "Långängens Gård",
-      address: "Långängsvägen 116, Lidingö",
+      address: "Långängens Gård, Lidingö",
     },
   ];
 
